6c04c48c025f74bd5ddbab01d1307d68e59550e5
Ikuo Degawa <degawa@ikuwow.com>
2018-04-01 19:55:34 +0900

    for

--- src/main/scala/Loop.scala
+++ src/main/scala/Loop.scala
@@ -1,5 +1,7 @@
 object Loop {
   def main(args: Array[String]): Unit = {
+
+    println("while:")
     var i = 0;
     while (i < 10) {
       println(i)
@@ -10,5 +12,12 @@ object Loop {
       println(i)
       i -= 1
     } while (i > 0)
+
+    println("for:")
+    val result = for (i <- 0 to 3; j <- 1 until 5; if i != j) yield s"$i, $j"
+    for (r <- result) {
+      println(r)
+    }
   }
+
 }
