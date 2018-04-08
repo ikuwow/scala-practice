4d5f3437bc3177f743a3fb21f94560b3372b198e
Ikuo Degawa <degawa@ikuwow.com>
2018-04-08 20:27:55 +0900

    packaged

---
+++ src/main/scala/Call.scala
@@ -0,0 +1,27 @@
+package com.ikuwow.scalapractice.main
+
+import com.ikuwow.scalapractice.model._
+
+object Call {
+  def sayHello(name: String = "Jigglipuff", age: Int = 14): Unit = { // Unit means "void"
+    println(s"Hello, $name. You are $age years old")
+  }
+
+  def main(args: Array[String]): Unit = {
+    sayHello("ikuwow", 27)
+    sayHello(age = 80, name = "Gold")
+    sayHello(age = 2)
+
+    // val user: User = new User
+    val tom = new User("tom")
+    println(tom.name)
+    tom.sayHi
+
+    val unnamed = new User
+    unnamed.sayHi
+
+    val bob = new AdminUser(name = "bob", age = 33)
+    bob.sayHello
+    bob.sayHi
+  }
+}
