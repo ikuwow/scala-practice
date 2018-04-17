234aa7b8bc76bbe16d7391f19f83f8a5123d6854
Ikuo Degawa <degawa@ikuwow.com>
2018-04-17 18:12:51 +0900

    List

--- src/main/scala/Other.scala
+++ src/main/scala/Other.scala
@@ -9,5 +9,15 @@ object Other {
     val (x, y) = swap(30, 20)
     println(x)
     println(y)
+
+    // Collection: Seq (List, ...), Set, Map
+
+    // val scores = List[Int](10, 20, 30)
+    // val scores = List(10, 20, 30)
+    val scores = 10 :: 20 :: 30 :: Nil
+    println(scores.length)
+    println(scores.isEmpty)
+    println(scores.head)
+    println(scores(2))
   }
 }
