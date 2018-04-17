8345f2b4c19a4398ca0113dfaa9da72c66c9cf25
Ikuo Degawa <degawa@ikuwow.com>
2018-04-17 18:09:50 +0900

    touple

--- src/main/scala/Other.scala
+++ src/main/scala/Other.scala
@@ -1,8 +1,13 @@
 object Other {
+  def swap(a: Int, b: Int) = (b, a)
+
   def main(args: Array[String]): Unit = {
     // touple
     val data = (12, "ikuwow", 60)
     println(data._1)
     println(data._3)
+    val (x, y) = swap(30, 20)
+    println(x)
+    println(y)
   }
 }
